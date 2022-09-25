cd681d6a075379f5461d791c0237bd12017d4394
Andrew PV <112729614+open230884@users.noreply.github.com>
2022-09-25 23:36:41 +0300

    с индексацией, тупит

--- Program.cs
+++ Program.cs
@@ -1,14 +1,13 @@
-﻿// Пишем прогу для расчета факториала новым способом
- double Factorial (int n)
- {
-    // 1! = 1
-    // 0! = 1
-    if (n == 1) return 1;
-    else return n * Factorial(n - 1);
- }
-for (int i = 1; i<40; i++)
+﻿int Fibonacci(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+
+
+for (int i = 1; i < 40; i++)
 {
 
-Console.WriteLine($"{i}! = {Factorial(i)}"); 
-
+Console.WriteLine(Fibonacci(i));
 }
