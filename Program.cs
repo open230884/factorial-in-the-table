790f117ae9bc998de0035e3f0e8c1df79b1c4fd2
open_x <open_x@inbox.ru>
2022-09-25 22:07:18 +0300

    доработал программу

--- Program.cs
+++ Program.cs
@@ -6,5 +6,9 @@
     if (n == 1) return 1;
     else return n * Factorial(n - 1);
  }
+for (int i = 1; i<40; i++)
+{
 
-Console.WriteLine(Factorial(3)); // 1*2*3 = 6
+Console.WriteLine($"{i}! = {Factorial(i)}"); 
+
+}
